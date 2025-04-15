555541361caa65660c5b80770a0dae1247c5a3cc
Timothy Ayegbede <timtop99@gmail.com>
2025-04-15 11:52:45 +0100

    added lenis scrolling feature

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 alert("This is working script");
+gsap.registerPlugin(ScrollTrigger);
+
+// Lenis Init
+let isMobile = false;
+
+function checkDevice() {
+  if (window.innerWidth <= 767) {
+    isMobile = true;
+  } else {
+    isMobile = false;
+  }
+}
+checkDevice();
+window.addEventListener("resize", checkDevice);
+
+//Smooth Scroll
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
